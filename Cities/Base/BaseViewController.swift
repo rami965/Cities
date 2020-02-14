6a887a763056c0e81a9dfaadf5db67e201f6e732
rami965 <ramisaid33@hotmail.com>
2020-02-14 19:58:34 +0200

    initialized main app window with coordinator. added the base view controller and the base presenter.

---
+++ Cities/Base/BaseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BaseViewController.swift
+//  Cities
+//
+//  Created by Rami on 2/14/20.
+//  Copyright © 2020 Rami Saeed. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
